e490c3f932ab21c6b2306cc963c0ae253d8c6ed1
Michal Gorzkowski <michal.g@dianet.pl>
2021-04-08 13:08:55 +0200

    pierwszy commit

---
+++ lekcja1/start.py
@@ -0,0 +1,10 @@
+#print("hello")
+
+myvar= 'hello Pycharm'
+myvar2=myvar
+print(myvar, myvar2)
+print(type(myvar), type(myvar2))
+
+print( 'Is value the same?' , myvar == myvar2 )
+print( 'Are te variables the same?' , myvar is myvar2)
+print(id(myvar), id(myvar2))
